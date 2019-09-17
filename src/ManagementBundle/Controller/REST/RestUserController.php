f6b7e7bd7ae98babebcb61aaec8ba0846e8357b1
Gintautas Plonis <gplonis@gmail.com>
2019-09-17 22:34:36 +0300

    Create new controller for user api

---
+++ src/ManagementBundle/Controller/REST/RestUserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ManagementBundle\Controller\REST;
+
+use Doctrine\ORM\EntityManager;
+use ManagementBundle\Repository\UserRepository;
+use ManagementBundle\Serializer\Serializer;
+use ManagementBundle\Serializer\UserNormalizer;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RestUserController
+{
+    private $serializer;
+    private $userNormalizer;
+    private $userRepository;
+    private $entityManager;
+
+    public function __construct(
+        Serializer $serializer,
+        UserNormalizer $userNormalizer,
+        UserRepository $userRepository,
+        EntityManager $entityManager
+    )
+    {
+        $this->serializer = $serializer;
+        $this->userNormalizer = $userNormalizer;
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function createAction(Request $request)
+    {
+        $requestData = $request->getContent();
+        $user = $this->serializer->deserialize($requestData, $this->userNormalizer);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        $this->userRepository->find($user);
+
+        return JsonResponse::fromJsonString(
+            $this->serializer->serialize($user, $this->userNormalizer),
+            Response::HTTP_CREATED
+        );
+    }
+}
