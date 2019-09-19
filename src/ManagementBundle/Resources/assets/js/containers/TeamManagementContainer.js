1ef30e0fbf9243d29446e906e6254c3c26eed3eb
Gintautas Plonis <gplonis@gmail.com>
2019-09-19 21:56:36 +0300

    Enable adding new team

--- src/ManagementBundle/Resources/assets/js/containers/TeamManagementContainer.js
+++ src/ManagementBundle/Resources/assets/js/containers/TeamManagementContainer.js
@@ -5,34 +5,57 @@ class TeamManagementContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            teams: []
-        }
+            teams: [],
+            title: '',
+            errors: [],
+        };
     }
 
     componentDidMount() {
         axios.get('/rest/v1/teams')
-            .then(res => {
-                this.setState({
-                    teams: res.data.data
-                });
-            })
-            .catch(err => console.log(err));
+            .then(res => this.setState({
+                teams: res.data.data
+            }))
+            .catch(err => console.log(err.response));
+    }
+
+    handleAdd() {
+        const { title, teams } = this.state;
+        axios.post('/rest/v1/teams', { title })
+            .then(res => this.setState({
+                teams: [...teams, res.data.data],
+                title: '',
+            }))
+            .catch(err => console.log(err.response));
+    }
+
+    handleTitleChange(e) {
+        this.setState({
+            title: e.target.value
+        });
     }
 
     render() {
+        const { teams, title } = this.state;
+
         return (
             <main className="team-container">
                 <aside className="team-container--half">
                     <h5>Add new team</h5>
                     <div className="form-group">
                         <div className="input-container">
-                            <input type="text" className="form-control" placeholder="Enter title"/>
-                            <button className="btn btn-primary">Add</button>
+                            <input
+                                onChange={e => this.handleTitleChange(e)}
+                                type="text"
+                                className="form-control"
+                                value={title}
+                                placeholder="Enter title"/>
+                            <button onClick={() => this.handleAdd()} className="btn btn-primary">Add</button>
                         </div>
                     </div>
                     <div className="list">
                         <ul className="list-group-flush">
-                            {this.state.teams.map(team => (
+                            {teams.map(team => (
                                 <li key={team.id} className="list-group-item">{team.title}</li>
                             ))}
                         </ul>
