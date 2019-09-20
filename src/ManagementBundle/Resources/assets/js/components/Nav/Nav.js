0902937304c046c4d0ca548d2afa190520b5c90a
Gintautas Plonis <gplonis@gmail.com>
2019-09-20 14:36:49 +0300

    Display admin nav links dynamically

--- src/ManagementBundle/Resources/assets/js/components/Nav/Nav.js
+++ src/ManagementBundle/Resources/assets/js/components/Nav/Nav.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import clsx from 'clsx';
+import {ROUTES} from '../../config';
 
-const Nav = () => (
-    <nav className="d-flex justify-content-around bg-light align-items-center p-4">
-        <div>Admin Panel</div>
-        <div>
-            <Link to="/users">Users</Link>
-            <Link to="/admin">Teams</Link>
-        </div>
-        <div>
-            <a className="text-decoration-none text-danger" href="/logout">Logout</a>
-        </div>
-    </nav>
-);
+class Nav extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePage: '/admin',
+        }
+    }
 
-export default React.memo(Nav);
+    handlePageChange(page) {
+        this.setState({ activePage: page });
+    }
+
+    render() {
+        const { activePage } = this.state;
+
+        return (
+            <nav className="d-flex justify-content-around bg-light align-items-center p-4">
+                <div>Admin Panel</div>
+                <div className="d-flex">
+                    {ROUTES.map(route => (
+                        <div
+                            key={route.link}
+                            className={clsx('mr-4', activePage === route.link ? 'bold' : '')}>
+                            <Link
+                                onClick={() => this.handlePageChange(route.link)}
+                                to={route.link}>
+                                {route.title}
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    <a className="text-decoration-none text-danger" href="/logout">Logout</a>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Nav;
