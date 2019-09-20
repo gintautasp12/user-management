669d33519524e544de024fecbc8c0798d0f46ba8
Gintautas Plonis <gplonis@gmail.com>
2019-09-20 15:49:30 +0300

    Add fixtures with user-team relations

--- src/ManagementBundle/DataFixtures/AppFixtures.php
+++ src/ManagementBundle/DataFixtures/AppFixtures.php
@@ -4,13 +4,35 @@ namespace ManagementBundle\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use ManagementBundle\Entity\Team;
 use ManagementBundle\Entity\User;
-use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     private $encoder;
+    private const USERS = [
+        ['name' => 'Mr. Bean'],
+        ['name' => 'Lorem ipsum'],
+        ['name' => 'Thor'],
+        ['name' => 'Susan Doe'],
+        ['name' => 'Tom'],
+        ['name' => 'Mark Zuckerberg'],
+        ['name' => 'Jon'],
+        ['name' => 'Peter'],
+        ['name' => 'Steve'],
+        ['name' => 'Ironman'],
+        ['name' => 'Hulk'],
+    ];
+    private const TEAMS = [
+        ['title' => 'Avengers'],
+        ['title' => 'Alpha'],
+        ['title' => 'Beta'],
+        ['title' => 'Gamma'],
+        ['title' => 'TeamA'],
+        ['title' => 'Office'],
+        ['title' => 'Delta'],
+    ];
 
     public function __construct(UserPasswordEncoderInterface $encoder)
     {
@@ -19,13 +41,46 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadTeams($manager);
+        $this->loadAdminUser($manager);
+
+        $manager->flush();
+    }
+
+    private function loadUsers(ObjectManager $manager)
+    {
+        foreach (self::USERS as $user) {
+            $userFixture = new User();
+            $userFixture->setName($user['name']);
+            $this->addReference($user['name'], $userFixture);
+            $manager->persist($userFixture);
+        }
+    }
+
+    private function loadTeams(ObjectManager $manager)
+    {
+        foreach (self::TEAMS as $team) {
+            $teamFixture = new Team();
+            $teamFixture->setTitle($team['title']);
+            for ($i = 0; $i < rand(0, count(self::USERS) - 1); $i++) {
+                /** @var User $user */
+                $user = $this->getReference(self::USERS[rand(0, count(self::USERS) - 1)]['name']);
+                if (!in_array($user, $teamFixture->getUsers())) {
+                    $teamFixture->addUser($user);
+                }
+            }
+            $manager->persist($teamFixture);
+        }
+    }
+
+    private function loadAdminUser(ObjectManager $manager)
+    {
         $user = new User();
         $user->setUsername('admin')
             ->setPassword($this->encoder->encodePassword($user, 'password'))
             ->setRoles(['ROLE_ADMIN'])
             ->setName('Admin');
-
         $manager->persist($user);
-        $manager->flush();
     }
 }
